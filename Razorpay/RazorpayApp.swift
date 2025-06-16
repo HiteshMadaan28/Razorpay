d9ca3428ccebc9c21202e4bfa4b86ec53881c711
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2025-06-16 13:05:34 +0530

    Initial Commit

---
+++ Razorpay/RazorpayApp.swift
@@ -0,0 +1,17 @@
+//
+//  RazorpayApp.swift
+//  Razorpay
+//
+//  Created by Hitesh Madaan on 16/06/25.
+//
+
+import SwiftUI
+
+@main
+struct RazorpayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
